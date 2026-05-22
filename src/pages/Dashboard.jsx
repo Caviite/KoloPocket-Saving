6b12c60444e6838c648619aab3339c3500127fd2
caviite <yomidave8682@gmail.com>
2026-05-22 20:46:13 +0100

    https commit

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Dashboard() {
   return (
     <div>
-      <h1>Welcome to your Kolo Dashboard</h1>
+      <h1>Welcome to your Kolo Dashboard Track Yout Money</h1>
     </div>
   )
 }
